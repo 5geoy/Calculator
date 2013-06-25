f4e868de10adac2e5b3a03667a9ffe2dc8ad50b4
iD Student <Student@idacademy-mac-1.princeton.edu>
2013-06-25 10:50:55 -0400

    initial commit

--- testApp/ViewController.h
+++ testApp/ViewController.h
@@ -8,6 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ViewController : UIViewController{
+    float firstNumber;
+    int operation;
+    int count;
+    float secondNumber;
+    BOOL op;
+    BOOL dec;
+    /*@property(x, y)*/IBOutlet UITextField *answer;
+    
+}
+-(IBAction)getNumber:(id)sender;
+-(IBAction)doMath:(id)sender;
+-(IBAction)getOperation:(id)sender;
+-(IBAction)clear:(id)sender;
+-(IBAction)decimal:(id)sender;
 @end
--- testApp/ViewController.m
+++ testApp/ViewController.m
@@ -7,16 +7,19 @@
 //
 
 #import "ViewController.h"
+#include "math.h"
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+//@synthesize answer;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    op = TRUE;
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -26,4 +29,79 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(IBAction)getNumber:(id)sender{
+    //NSLog(@"%ld", (long)op);
+    if(op)
+    {
+        if(dec)
+        {
+            count++;
+            firstNumber = firstNumber+ pow(10, -count)*[sender tag];
+            answer.text = [NSString stringWithFormat:@"%5.2f", firstNumber];
+        }
+        else{
+            firstNumber = firstNumber*10 + (float)[sender tag];
+            answer.text = [NSString stringWithFormat:@"%3.2f", firstNumber];
+        }
+    }
+    else 
+    {
+        if(dec)
+        {
+            count++;
+            secondNumber = secondNumber+ pow(10, -count)*[sender tag];
+            answer.text = [NSString stringWithFormat:@"%5.2f", secondNumber];
+        }
+        else
+        {
+            secondNumber = secondNumber*10+ (float)[sender tag];
+            answer.text = [NSString stringWithFormat:@"%3.2f", secondNumber];
+        }
+    }
+    
+}
+-(IBAction)doMath:(id)sender
+{
+    switch(operation)
+    {
+        case 0:
+            answer.text = [NSString stringWithFormat:@"%6.2f", (firstNumber + secondNumber)];
+            break;
+        case 1:
+            answer.text = [NSString stringWithFormat:@"%3.2f", (firstNumber - secondNumber)];
+            break;
+        case 2:
+            answer.text = [NSString stringWithFormat:@"%3.2f", (firstNumber * secondNumber)];
+            break;
+        case 3:
+            answer.text = [NSString stringWithFormat:@"%3.2f", (firstNumber / secondNumber)];
+            break;
+    }
+    firstNumber = 0.0;
+    secondNumber = 0.0;
+    op = TRUE;
+    dec = FALSE;
+}
+-(IBAction)getOperation:(id)sender
+{
+    operation = [sender tag];
+    answer.text = [NSString stringWithFormat:@""];
+    op = FALSE;
+    count = 0;
+    dec = FALSE;
+}
+
+-(IBAction)clear:(id)sender
+{
+    answer.text = [NSString stringWithFormat:@""];
+    firstNumber = 0.0;
+    secondNumber = 0.0;
+    op = TRUE;
+    dec = FALSE;
+}
+
+-(IBAction)decimal:(id)sender
+{
+    dec = TRUE;
+}
 @end
